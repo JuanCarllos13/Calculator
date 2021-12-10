1ff1cd20cbe2833adee8aeb313d0ada3115982d3
Juan Carlosl <juan2017amaral@gmail.com>
2021-12-10 18:18:10 -0300

    Files

---
+++ script.js
@@ -0,0 +1,24 @@
+function insert(number) {
+   let num = document.getElementById('resultado').innerHTML
+   document.getElementById('resultado').innerHTML = num + number
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function back() {
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML;
+
+    if(resultado){
+    document.getElementById('resultado').innerHTML = eval(resultado);
+    }else {
+        document.getElementById('resultado').innerHTML = 'Nada...'
+    }
+
+}
